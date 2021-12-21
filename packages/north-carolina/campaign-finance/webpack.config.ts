e2be30510d6121660438b7f44f172bbb34cc9445
Chris Birk <cmbirk@gmail.com>
2021-12-20 20:43:23 -0500

    add babel config, webpack for campaign finance

---
+++ packages/north-carolina/campaign-finance/webpack.config.ts
@@ -0,0 +1,25 @@
+import { Configuration } from 'webpack'
+import path from 'path'
+
+const config: Configuration = {
+  mode: "development",
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+export default config
