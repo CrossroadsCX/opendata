53a198dbbeef7c8d46f3ead7cb51da05c5cc4839
Chris Birk <cmbirk@gmail.com>
2022-02-02 02:00:56 -0600

    add buildNotifications cloud function triggered from the cloud-builds topic

--- packages/north-carolina/campaign-finance/index.js
+++ packages/north-carolina/campaign-finance/index.js
@@ -1,6 +1,15 @@
+const { buildNotifications } = require('./dist/cjs/buildNotifications')
 const { reportsScraper } = require('./dist/cjs/reportsScraper')
 const { transactionsScraper } = require('./dist/cjs/transactionsScraper')
+const { transactionsSnowpipe } = require('./dist/cjs/transactionsSnowpipe')
 const { transactionsStaging } = require('./dist/cjs/transactionsStaging')
 const { triggerTransactionsScraper } = require('./dist/cjs/triggerTransactionsScraper')
 
-module.exports = { reportsScraper, transactionsScraper, transactionsStaging, triggerTransactionsScraper }
+module.exports = {
+  buildNotifications,
+  reportsScraper,
+  transactionsScraper,
+  transactionsSnowpipe,
+  transactionsStaging,
+  triggerTransactionsScraper,
+}
--- packages/north-carolina/campaign-finance/src/buildNotifications.ts
+++ packages/north-carolina/campaign-finance/src/buildNotifications.ts
@@ -1,12 +1,34 @@
 import type { Context, LegacyEvent } from '@google-cloud/functions-framework'
 import { logger, createSlackLogger } from './logger'
 
-export const buildNotifications = async (event: LegacyEvent, context: Context) => {
-  const slackLogger = await createSlackLogger()
-  logger.info('Event: ', event)
-  logger.info('Context: ', context)
+type BuildEvent = {
+  attributes: Record<string, unknown>
+  data: string;
+}
 
-  slackLogger.info(`Cloud Function Deployed.`)
+type BuildEventData = {
+  status: string;
+  substitutions: {
+    _GOOGLE_FUNCTION_TARGET: string;
+  }
+  // More attributes https://cloud.google.com/build/docs/api/reference/rest/v1/projects.builds
+}
+
+export const buildNotifications = async (event: BuildEvent, context: Context) => {
+  const slackLogger = await createSlackLogger()
+  try {
+    const { data  } = event
+    const dataString = Buffer.from(data, 'base64').toString()
+    const dataObject: BuildEventData = JSON.parse(dataString)
+
+    const { status, substitutions: { _GOOGLE_FUNCTION_TARGET }} = dataObject
+
+    slackLogger.info(`Cloud function built: ${_GOOGLE_FUNCTION_TARGET} :: ${status}`)
+  } catch (err) {
+    logger.error(err)
+    slackLogger.error(err)
+    throw err
+  }
 
   return
 }
